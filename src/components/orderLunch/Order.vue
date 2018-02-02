33ff3d6f05a61ba54da95e731b0e2cbd5b62ca23
strangerDemon <735359571@qq.com>
2018-02-02 14:38:52 +0800

    update

--- src/components/orderLunch/Order.vue
+++ src/components/orderLunch/Order.vue
@@ -13,7 +13,7 @@
       <div class="prompt">就餐时间：下一个工作日中午</div>
       <div class="prompt">点餐时间：早 {{systemParam.startTime}}点 至 晚 {{systemParam.endTime}}点</div>
     </div>
-    <div class="admire" v-show="isAdmire">金额不足，请先充值</div>
+    <!--<div class="admire" v-show="isAdmire">金额不足，请先充值</div>-->
     <div class="admire" v-show="showNoUser">没有点餐账户，请联系管理员添加</div>
     <div class="menuDiv">
       <el-card class="box-card">
@@ -36,7 +36,7 @@
       </mt-cell>
       <span class="space"></span>
     </div>
-    <mt-button v-show="!isAdmire&&!showNoUser&&isOrderOrNot" class="saveButton" type="primary" size="large" @click.native="commit">点餐</mt-button>
+    <mt-button v-show="!showNoUser&&isOrderOrNot" class="saveButton" type="primary" size="large" @click.native="commit">点餐</mt-button>
     <mt-popup v-model="dialoglunchType" popup-transition="popup-fade" style="width: 80%;">
       <mt-radio title="点餐类别" align="right" v-model="lunchType" :options="lunchTypeOptions">
       </mt-radio>
@@ -48,368 +48,329 @@
   </div>
 </template>
 <script>
-import * as AnalysisRequestParam from "@/utils/AnalysisRequestParam";
-export default {
-  name: "Order",
-  directives: {},
-  components: {},
-  data() {
-    return {
-      isAdmire: false,
-      showNoUser: true,
-      isOrderOrNot: false,
-      isShowMenu: true,
-      dialoglunchType: false,
-      orderTimes: 0,
-      lunchType: "c",
-      lunchTypeTimes: [],
-      lunchTypeOptions: [
-        {
-          label: "a餐",
-          value: "1",
-          disabled: true
-        },
-        {
-          label: "b餐",
-          value: "2",
-          disabled: true
-        },
-        {
-          label: "c餐",
-          value: "3"
-        }
-      ],
-      disabledLunch: [],
-      moneyA: 0,
-      moneyB: 0,
-      moneyC: 0
-    };
-  },
-  props: {},
-  computed: {
-    code() {
-      return this.$store.state.init.code;
+  import * as AnalysisRequestParam from "@/utils/AnalysisRequestParam";
+  export default {
+    name: "Order",
+    directives: {},
+    components: {},
+    data() {
+      return {
+        isAdmire: false,
+        showNoUser: true,
+        isOrderOrNot: false,
+        isShowMenu: true,
+        dialoglunchType: false,
+        orderTimes: 0,
+        lunchType: "c",
+        lunchTypeTimes: [],
+        lunchTypeOptions: [{
+            label: "a餐",
+            value: "1",
+            disabled: true
+          },
+          {
+            label: "b餐",
+            value: "2",
+            disabled: true
+          },
+          {
+            label: "c餐",
+            value: "3"
+          }
+        ],
+      };
     },
-    systemParam() {
-      return this.$store.state.init.systemParam;
+    props: {},
+    computed: {
+      code() {
+        return this.$store.state.init.code;
+      },
+      systemParam() {
+        return this.$store.state.init.systemParam;
+      },
+      userInfo() {
+        return this.$store.state.init.userInfo;
+      },
+      menuList() {
+        return this.$store.state.init.menuList;
+      },
+      orderList() {
+        return this.$store.state.order.orderList;
+      }
     },
-    userInfo() {
-      return this.$store.state.init.userInfo;
-    },
-    menuList() {
-      return this.$store.state.init.menuList;
-    },
-    orderList() {
-      return this.$store.state.order.orderList;
-    }
-  },
-  watch: {
-    orderList(list) {
-      let vm = this;
+    watch: {
+      orderList(list) {
+        let vm = this;
 
-      if (vm.systemParam != null) {
-        vm.updatelunchTypeTimes();
-      }
-      if (vm.userInfo != null) {
-        vm.updateIsAdmire();
-      }
-    },
-    systemParam(systemParam) {
-      let vm = this;
-      if (!systemParam.isOrderOrNot) {
-        MessageBox.alert("点餐程序已关闭", "提示");
-        vm.isOrderOrNot = false;
-      } else {
-        vm.isOrderOrNot = true;
-      }
-      vm.moneyA = systemParam.lunch[0].money;
-      vm.moneyB = systemParam.lunch[1].money;
-      vm.moneyC = systemParam.lunch[2].money;
-      vm.disabledLunch = [];
-      vm.disabledLunch.push({
-        label: systemParam.lunch[0].label,
-        value: systemParam.lunch[0].value,
-        money: vm.moneyA,
-        disabled: true
-      });
-      vm.disabledLunch.push({
-        label: systemParam.lunch[1].label,
-        value: systemParam.lunch[1].value,
-        money: vm.moneyB,
-        disabled: true
-      });
-      vm.disabledLunch.push({
-        label: systemParam.lunch[2].label,
-        value: systemParam.lunch[2].value,
-        money: vm.moneyC,
-        disabled: true
-      });
-      if (vm.orderList != null) {
-        vm.updatelunchTypeTimes();
-      }
-    },
-    userInfo(userInfo) {
-      let vm = this;
-      vm.showNoUser = false;
-      if (vm.orderList != null) {
-        vm.updateIsAdmire();
-        vm.updatelunchTypeTimes();
-      }
-    }
-  },
-  methods: {
-    updateIsAdmire() {
-      let vm = this;
-      let balance = vm.userInfo.balance;
-      if (
-        (vm.userInfo.userType == "a" &&
-          ((vm.lunchTypeTimes[0] == 0 && balance < vm.moneyA) ||
-            (vm.lunchTypeTimes[0] > 0 &&
-              balance < vm.moneyB &&
-              balance < vm.moneyC))) ||
-        (vm.userInfo.userType == "b" &&
-          ((vm.lunchTypeTimes[1] == 0 && balance < vm.moneyB) ||
-            (vm.lunchTypeTimes[1] > 0 && balance < vm.moneyC))) ||
-        (vm.userInfo.userType == "c" && balance < vm.moneyC)
-      ) {
-        vm.isAdmire = true;
-      } else {
-        vm.isAdmire = false;
-      }
-    },
-    updatelunchTypeTimes() {
-      let vm = this;
-      vm.orderTimes = 0;
-      vm.lunchTypeTimes = new Array(3).fill(0);
-      for (let i = 0; i < vm.orderList.length; i++) {
-        if (vm.orderList[i].orderType > 0) {
-          vm.orderTimes++;
+        if (vm.systemParam != null) {
+          vm.updatelunchTypeTimes();
         }
-        if (vm.orderList[i].orderType == vm.systemParam.lunch[0].value) {
-          vm.lunchTypeTimes[0]++;
-        } else if (vm.orderList[i].orderType == vm.systemParam.lunch[1].value) {
-          vm.lunchTypeTimes[1]++;
-        } else if (vm.orderList[i].orderType == vm.systemParam.lunch[2].value) {
-          vm.lunchTypeTimes[2]++;
+        // if (vm.userInfo != null) {
+        //   vm.updateIsAdmire();
+        // }
+      },
+      systemParam(systemParam) {
+        let vm = this;
+        if (!systemParam.isOrderOrNot) {
+          MessageBox.alert("点餐程序已关闭", "提示");
+          vm.isOrderOrNot = false;
+        } else {
+          vm.isOrderOrNot = true;
+        }
+        if (vm.orderList != null) {
+          vm.updatelunchTypeTimes();
+        }
+      },
+      userInfo(userInfo) {
+        let vm = this;
+        vm.showNoUser = false;
+        if (vm.orderList != null) {
+          // vm.updateIsAdmire();
+          vm.updatelunchTypeTimes();
         }
       }
     },
-    cancel(orderId) {
-      let vm = this;
-      if (!vm.checkDate()) {
-        MessageBox.alert(
-          "只能在 " +
+    methods: {
+      updateIsAdmire() {
+        let vm = this;
+        let balance = vm.userInfo.balance;
+
+      },
+      updatelunchTypeTimes() {
+        let vm = this;
+        vm.orderTimes = 0;
+        vm.lunchTypeTimes = new Array(vm.systemParam.lunch.length).fill(0);
+        for (let i = 0; i < vm.orderList.length; i++) {
+          if (vm.orderList[i].orderType > 0) {
+            vm.orderTimes++;
+          }
+          for (let j = 0, length = vm.systemParam.lunch.length; j < length; j++) {
+            if (vm.orderList[i].orderType == vm.systemParam.lunch[j].value) {
+              vm.lunchTypeTimes[j]++;
+            }
+          }
+        }
+      },
+      cancel(orderId) {
+        let vm = this;
+        if (!vm.checkDate()) {
+          MessageBox.alert(
+            "只能在 " +
             vm.systemParam.startTime +
             " 至 " +
             vm.systemParam.endTime +
             " 间退餐",
-          "提示"
-        );
-        return;
-      }
-      MessageBox.confirm("确定取消订餐?").then(action => {
-        vm.$store.commit("cancle", {
-          code: vm.code,
-          Id: orderId,
-          func: function() {
-            vm.requestToday(100);
-          }
+            "提示"
+          );
+          return;
+        }
+        MessageBox.confirm("确定取消订餐?").then(action => {
+          vm.$store.commit("cancle", {
+            code: vm.code,
+            Id: orderId,
+            func: function() {
+              vm.requestToday(100);
+            }
+          });
         });
-      });
-    },
-    checkDate() {
-      let vm = this;
-      var current = new Date(); //当前时间
-      var hour = current.getHours();
-      if (hour >= vm.systemParam.endTime || hour < vm.systemParam.startTime) {
-        return false;
-      }
-      return true;
-    },
-    commit() {
-      let vm = this;
-      if (!vm.checkDate()) {
-        MessageBox.alert(
-          "只能在 " +
+      },
+      checkDate() {
+        let vm = this;
+        var current = new Date(); //当前时间
+        var hour = current.getHours();
+        if (hour >= vm.systemParam.endTime || hour < vm.systemParam.startTime) {
+          return false;
+        }
+        return true;
+      },
+      commit() {
+        let vm = this;
+        if (!vm.checkDate()) {
+          MessageBox.alert(
+            "只能在 " +
             vm.systemParam.startTime +
             " 至 " +
             vm.systemParam.endTime +
             " 间点餐",
-          "提示"
-        );
-        return;
-      }
-      if (vm.orderTimes > 0) {
-        MessageBox.confirm("今日已点餐" + vm.orderTimes + "次,是否继续点餐?")
-          .then(action => {
-            vm.setDialoglunchType();
-          })
-          .catch(error => {});
-      } else {
-        vm.setDialoglunchType();
+            "提示"
+          );
+          return;
+        }
+        if (vm.orderTimes > 0) {
+          MessageBox.confirm("今日已点餐" + vm.orderTimes + "次,是否继续点餐?")
+            .then(action => {
+              vm.setDialoglunchType();
+            })
+            .catch(error => {});
+        } else {
+          vm.setDialoglunchType();
+        }
+      },
+      setDialoglunchType() {
+        let vm = this;
+        vm.lunchTypeOptions = [];
+        vm.lunchType = "c";
+        let balance = vm.userInfo.balance;
+        let canOrder = false;
+        for (let i = 0, length = vm.systemParam.lunch.length; i < length; i++) {
+          if (
+            vm.lunchTypeTimes[i] >= vm.systemParam.lunch[i].maxOrderTimes ||
+            vm.userInfo.userType > vm.systemParam.lunch[i].value ||
+            balance < vm.systemParam.lunch[i].money
+          ) {
+            vm.lunchTypeOptions.push({
+              value: vm.systemParam.lunch[i].value,
+              label: vm.systemParam.lunch[i].label,
+              disabled: true
+            });
+          } else {
+            vm.lunchTypeOptions.push(vm.systemParam.lunch[i]);
+            canOrder = true;
+          }
+        }
+        if (canOrder) {
+          vm.dialoglunchType = true;
+        } else {
+          MessageBox.alert("无法点餐（点餐次数已满或金额不足）");
+        }
+      },
+      commitAction() {
+        let vm = this;
+        vm.dialoglunchType = false;
+        let isSelect = false;
+        for (let i = 0, length = vm.systemParam.lunch.length; i < length; i++) {
+          if (vm.systemParam.lunch[i].value == vm.lunchType) {
+            isSelect = true;
+          }
+        }
+        if (!isSelect) {
+          MessageBox.alert("请选择订餐类别", "提示");
+          return;
+        }
+        vm.$store.commit("order", {
+          code: vm.code,
+          orderType: vm.lunchType,
+          func: function() {
+            vm.requestToday(100);
+          }
+        });
+      },
+      requestToday(times) {
+        let vm = this;
+        setTimeout(function() {
+          vm.$store.commit("getUserInfo", { code: vm.code });
+        }, times);
+        let today = new Date().toLocaleDateString();
+        setTimeout(function() {
+          vm.$store.commit("getOrderList", {
+            name: "null",
+            code: vm.code,
+            startDate: today,
+            endDate: today,
+            orderType: 0,
+            changeType: 2,
+            page: 0,
+            isCancle: 1
+          });
+        }, times * 2);
       }
     },
-    setDialoglunchType() {
+    beforeCreate() {},
+    created() {},
+    destroyed() {},
+    mounted() {
+      Indicator.open({
+        text: "加载中...",
+        spinnerType: "fading-circle"
+      });
       let vm = this;
-      vm.dialoglunchType = true;
-      vm.lunchTypeOptions = [];
-      vm.lunchType = "c";
-      let balance = vm.userInfo.balance;
-      if (
-        vm.lunchTypeTimes[0] >= 1 ||
-        vm.userInfo.userType != "1" ||
-        balance < vm.moneyA
-      ) {
-        vm.lunchTypeOptions.push(vm.disabledLunch[0]);
-      } else {
-        vm.lunchTypeOptions.push(vm.systemParam.lunch[0]);
-      }
-      if (
-        vm.lunchTypeTimes[1] >= 1 ||
-        vm.userInfo.userType == "3" ||
-        balance < vm.moneyB
-      ) {
-        //c 类用户 或者不足b餐的金额
-        vm.lunchTypeOptions.push(vm.disabledLunch[1]);
-      } else {
-        vm.lunchTypeOptions.push(vm.systemParam.lunch[1]);
-      }
-      if (balance < vm.moneyC) {
-        vm.lunchTypeOptions.push(vm.disabledLunch[2]);
-      } else {
-        vm.lunchTypeOptions.push(vm.systemParam.lunch[2]);
-      }
-    },
-    commitAction() {
-      let vm = this;
-      vm.dialoglunchType = false;
-      if (!/^[0-3]*$/.test(vm.lunchType)) {
-        MessageBox.alert("请选择订餐类别", "提示");
-        return;
-      }
-      vm.$store.commit("order", {
+      vm.$store.commit("getSystemParam", {
         code: vm.code,
-        orderType: vm.lunchType,
         func: function() {
-          vm.requestToday(100);
+          vm.requestToday(500);
+          vm.$store.commit("getMeunList", { code: vm.code });
+          Indicator.close();
         }
       });
-    },
-    requestToday(times) {
-      let vm = this;
-      setTimeout(function() {
-        vm.$store.commit("getUserInfo", { code: vm.code });
-      }, times);
-      let today = new Date().toLocaleDateString();
-      setTimeout(function() {
-        vm.$store.commit("getOrderList", {
-          name: "null",
-          code: vm.code,
-          startDate: today,
-          endDate: today,
-          orderType: 0,
-          changeType: 2,
-          page: 0,
-          isCancle: 1
-        });
-      }, times * 2);
     }
-  },
-  beforeCreate() {},
-  created() {},
-  destroyed() {},
-  mounted() {
-    Indicator.open({
-      text: "加载中...",
-      spinnerType: "fading-circle"
-    });
-    let vm = this;
-    vm.$store.commit("getSystemParam", {
-      code: vm.code,
-      func: function() {
-        vm.requestToday(500);
-        vm.$store.commit("getMeunList", { code: vm.code });
-        Indicator.close();
-      }
-    });
-  }
-};
+  };
 </script>
 <style lang="css"
        scoped>
-.order {
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  font-size: 18px;
-  scroll-behavior: auto;
-}
+  .order {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    font-size: 18px;
+    scroll-behavior: auto;
+  }
 
-.userInfoDiv {
-  text-align: center;
-  margin: 13px 8px 8px;
-}
+  .userInfoDiv {
+    text-align: center;
+    margin: 13px 8px 8px;
+  }
 
-.userDiv {
-  display: block;
-  margin: 5px 0;
-  position: relative;
-}
+  .userDiv {
+    display: block;
+    margin: 5px 0;
+    position: relative;
+  }
 
-.balance,
-.username {
-  font-family: fantasy;
-}
+  .balance,
+  .username {
+    font-family: fantasy;
+  }
 
-.promptDiv {
-  text-align: center;
-  margin: 5px 0;
-  position: relative;
-}
+  .promptDiv {
+    text-align: center;
+    margin: 5px 0;
+    position: relative;
+  }
 
-.prompt {
-  margin: 3px 0;
-}
+  .prompt {
+    margin: 3px 0;
+  }
 
-.admire {
-  text-align: center;
-  font-size: 16px;
-  color: red;
-  margin: 5px;
-}
-/* 菜品列表 */
+  .admire {
+    text-align: center;
+    font-size: 16px;
+    color: red;
+    margin: 5px;
+  }
 
-.clearfix {
-  font-size: 20px;
-  margin: 5px 10px;
-}
+  /* 菜品列表 */
 
-.dish {
-  position: relative;
-  left: 5vw;
-  margin: 2.5px 0;
-  width: 29%;
-  display: inline-table;
-}
+  .clearfix {
+    font-size: 20px;
+    margin: 5px 10px;
+  }
 
-.isShowMenuButton {
-  float: right;
-}
+  .dish {
+    position: relative;
+    left: 5vw;
+    margin: 2.5px 0;
+    width: 29%;
+    display: inline-table;
+  }
 
-.saveButton {
-  position: fixed;
-  bottom: 0;
-  min-height: 60px;
-  border-radius: 0px;
-}
+  .isShowMenuButton {
+    float: right;
+  }
 
-.space {
-  width: 100%;
-  height: 60px;
-  position: absolute;
-}
+  .saveButton {
+    position: fixed;
+    bottom: 0;
+    min-height: 60px;
+    border-radius: 0px;
+  }
 
-.buttonText {
-  cursor: pointer;
-}
+  .space {
+    width: 100%;
+    height: 60px;
+    position: absolute;
+  }
+
+  .buttonText {
+    cursor: pointer;
+  }
 </style>
